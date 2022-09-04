c0b286fb82d4939782ec341bbe9bf3f14ce987da
hsl <he429855@gmail.com>
2022-09-04 16:18:37 +0800

    Initial commit

---
+++ myFunctions.go
@@ -0,0 +1,22 @@
+package myFunctions
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func SAllTypr(iV interface{}) {
+	// 这里的 v 是类型的值
+	switch nameiV := iV.(type) {
+	case bool:
+		fmt.Printf("bool\n")
+	case string:
+		fmt.Printf("string\n")
+	case int, int64:
+		fmt.Printf("int or int 64\n")
+	case float32, float64:
+		fmt.Printf("float32 or 64\n")
+	default:
+		fmt.Printf("what Unknown is %v\n", reflect.TypeOf(nameiV))
+	}
+}
